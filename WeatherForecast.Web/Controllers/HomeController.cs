a69051656de1403fb1555a300d89019dbe3b670e
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2023-12-11 00:44:04 +0700

    Add project files.

---
+++ WeatherForecast.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherForecast.Web.Models;
+using WeatherForecast.Web.Services;
+
+namespace WeatherForecast.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWeatherForecastHttpService _weatherForecastHttpService;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(
+            IWeatherForecastHttpService weatherForecastHttpService,
+            ILogger<HomeController> logger)
+        {
+            _weatherForecastHttpService = weatherForecastHttpService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            _logger.LogInformation("Fetching weather forecast api...");
+            var response = await _weatherForecastHttpService.GetWeatherForecasts();
+            _logger.LogInformation($"Weather forecast data contains {response.Count()}");
+
+            return View(response);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherForecast.Web/Services/WeatherForecastHttpService.cs
@@ -0,0 +1,24 @@
+﻿using WeatherForecast.Shared;
+
+namespace WeatherForecast.Web.Services
+{
+    public interface IWeatherForecastHttpService
+    {
+        Task<IEnumerable<WeatherForecastModel>> GetWeatherForecasts();
+    }
+    public class WeatherForecastHttpService : IWeatherForecastHttpService
+    {
+        private readonly HttpClient _httpClient;
+        public WeatherForecastHttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        async Task<IEnumerable<WeatherForecastModel>> IWeatherForecastHttpService.GetWeatherForecasts()
+        {
+            IEnumerable<WeatherForecastModel>? forecasts =
+                await _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecastModel>>("/WeatherForecast");
+            return forecasts ?? Enumerable.Empty<WeatherForecastModel>();
+        }
+    }
+}
